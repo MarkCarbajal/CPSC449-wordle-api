fd1b8331b890cf3381d1ebbf4194ca7688433b55
MarkCarbajal <markc9090@csu.fullerton.edu>
2022-10-20 21:42:43 -0700

    added sql schema

---
+++ src/wordle.sql
@@ -0,0 +1,22 @@
+PRAGMA FOREIGN_KEYS=ON;
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INTEGER primary key,
+    username VARCHAR,
+    password VARCHAR
+);
+
+DROP TABLE IF EXISTS game;
+CREATE TABLE game(
+    id INTEGER primary key,
+    id_user INT references user(id),
+    correct_word VARCHAR,
+    game_win BOOLEAN,
+    guess_num INT
+);
+
+/*Add tables for input checking*/
+
+COMMIT;
